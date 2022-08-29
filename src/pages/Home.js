8348caabb5d614f35a7926842ed79d18e67a1a2a
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-08-29 15:49:15 +0600

    home page UI almost complete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,19 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <main>
-            <h2>this is home page</h2>
+        <main className='max-w-6xl mx-auto px-3 md:px-0'>
+            <div class="grid lg:grid-cols-2 bg-neutral text-secondary rounded-none w-full card-h mt-7">
+                <div>
+                    <img src="https://placeimg.com/400/400/arch" alt="profile_img" className='profile_img' />
+                </div>
+                <div class="text-secondary p-4">
+                    <h2>New album is released!</h2>
+                    <p>Click the button to listen on Spotiwhy app.</p>
+                    <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Listen</button>
+                    </div>
+                </div>
+            </div>
         </main>
     );
 };
