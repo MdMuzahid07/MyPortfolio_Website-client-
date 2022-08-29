1a9a1e9ceab28c7c32a19b5df9aadacf8a9ba6c4
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-08-29 23:30:10 +0600

    technologies card UI complete

--- src/components/About/Skills.js
+++ src/components/About/Skills.js
@@ -4,134 +4,156 @@ const Skills = () => {
     return (
         <section className='text-secondary my-20'>
             <h2 className='text-3xl font-bold underline mb-7'>My Skills</h2>
-            <div className="grid lg:grid-cols-2 bg-primary text-secondary rounded w-full p-4 mt-7">
-                <div>
-                    <h2 className='text-3xl'>Technologies i work with</h2>
-                    <p className='text-md font-bold'>{`
-                            <> I'm a Web Developer, Works with MERN Stack, Front-end ,Back-end Development, and WordPress Website Design. My future goal is to be a full-stack native app developer. and I want to be a master in JavaScript.My first rule is, to be a good person,then a good Developer. </>
-                            `}</p>
-                </div>
-                <div className="grid md:grid-cols-8 gap-2">
-                    <div class="avatar shadow_white">
-                        <div class="w-20 rounded">
-                            <img src="https://placeimg.com/192/192/people" />
+            <div className="bg-primary text-secondary rounded w-full md:p-4 mt-7">
+
+                <div className="grid md:grid-cols-6 lg:grid-cols-12 gap-3 bg-accent">
+
+                    <div>
+                        <div class="shadow_white rounded flex justify-center items-center p-2 md:p-0">
+                            <img src="https://placeimg.com/192/192/people" className='w-auto' alt="technologies" />
                         </div>
                     </div>
-                    <div class="avatar shadow_white">
-                        <div class="w-20 rounded">
-                            <img src="https://placeimg.com/192/192/people" />
+
+                    <div>
+                        <div class="shadow_white rounded flex justify-center items-center p-2 md:p-0">
+                            <img src="https://placeimg.com/192/192/people" className='w-auto' alt="technologies" />
                         </div>
                     </div>
-                    <div class="avatar shadow_white">
-                        <div class="w-20 rounded">
-                            <img src="https://placeimg.com/192/192/people" />
+
+                    <div>
+                        <div class="shadow_white rounded flex justify-center items-center p-2 md:p-0">
+                            <img src="https://placeimg.com/192/192/people" className='w-auto' alt="technologies" />
                         </div>
                     </div>
-                    <div class="avatar shadow_white">
-                        <div class="w-20 rounded">
-                            <img src="https://placeimg.com/192/192/people" />
+
+                    <div>
+                        <div class="shadow_white rounded flex justify-center items-center p-2 md:p-0">
+                            <img src="https://placeimg.com/192/192/people" className='w-auto' alt="technologies" />
                         </div>
                     </div>
-                    <div class="avatar shadow_white">
-                        <div class="w-20 rounded">
-                            <img src="https://placeimg.com/192/192/people" />
+
+                    <div>
+                        <div class="shadow_white rounded flex justify-center items-center p-2 md:p-0">
+                            <img src="https://placeimg.com/192/192/people" className='w-auto' alt="technologies" />
                         </div>
                     </div>
-                    <div class="avatar shadow_white">
-                        <div class="w-20 rounded">
-                            <img src="https://placeimg.com/192/192/people" />
+
+                    <div>
+                        <div class="shadow_white rounded flex justify-center items-center p-2 md:p-0">
+                            <img src="https://placeimg.com/192/192/people" className='w-auto' alt="technologies" />
                         </div>
                     </div>
-                    <div class="avatar shadow_white">
-                        <div class="w-20 rounded">
-                            <img src="https://placeimg.com/192/192/people" />
+
+                    <div>
+                        <div class="shadow_white rounded flex justify-center items-center p-2 md:p-0">
+                            <img src="https://placeimg.com/192/192/people" className='w-auto' alt="technologies" />
                         </div>
                     </div>
-                    <div class="avatar shadow_white">
-                        <div class="w-20 rounded">
-                            <img src="https://placeimg.com/192/192/people" />
+
+                    <div>
+                        <div class="shadow_white rounded flex justify-center items-center p-2 md:p-0">
+                            <img src="https://placeimg.com/192/192/people" className='w-auto' alt="technologies" />
                         </div>
                     </div>
-                    <div class="avatar shadow_white">
-                        <div class="w-20 rounded">
-                            <img src="https://placeimg.com/192/192/people" />
+
+                    <div>
+                        <div class="shadow_white rounded flex justify-center items-center p-2 md:p-0">
+                            <img src="https://placeimg.com/192/192/people" className='w-auto' alt="technologies" />
                         </div>
                     </div>
-                    <div class="avatar shadow_white">
-                        <div class="w-20 rounded">
-                            <img src="https://placeimg.com/192/192/people" />
+
+                    <div>
+                        <div class="shadow_white rounded flex justify-center items-center p-2 md:p-0">
+                            <img src="https://placeimg.com/192/192/people" className='w-auto' alt="technologies" />
                         </div>
                     </div>
-                    <div class="avatar shadow_white">
-                        <div class="w-20 rounded">
-                            <img src="https://placeimg.com/192/192/people" />
+
+                    <div>
+                        <div class="shadow_white rounded flex justify-center items-center p-2 md:p-0">
+                            <img src="https://placeimg.com/192/192/people" className='w-auto' alt="technologies" />
                         </div>
                     </div>
-                    <div class="avatar shadow_white">
-                        <div class="w-20 rounded">
-                            <img src="https://placeimg.com/192/192/people" />
+
+                    <div>
+                        <div class="shadow_white rounded flex justify-center items-center p-2 md:p-0">
+                            <img src="https://placeimg.com/192/192/people" className='w-auto' alt="technologies" />
                         </div>
                     </div>
-                    <div class="avatar shadow_white">
-                        <div class="w-20 rounded">
-                            <img src="https://placeimg.com/192/192/people" />
+
+
+                    <div>
+                        <div class="shadow_white rounded flex justify-center items-center p-2 md:p-0">
+                            <img src="https://placeimg.com/192/192/people" className='w-auto' alt="technologies" />
                         </div>
                     </div>
-                    <div class="avatar shadow_white">
-                        <div class="w-20 rounded">
-                            <img src="https://placeimg.com/192/192/people" />
+
+                    <div>
+                        <div class="shadow_white rounded flex justify-center items-center p-2 md:p-0">
+                            <img src="https://placeimg.com/192/192/people" className='w-auto' alt="technologies" />
                         </div>
                     </div>
-                    <div class="avatar shadow_white">
-                        <div class="w-20 rounded">
-                            <img src="https://placeimg.com/192/192/people" />
+
+                    <div>
+                        <div class="shadow_white rounded flex justify-center items-center p-2 md:p-0">
+                            <img src="https://placeimg.com/192/192/people" className='w-auto' alt="technologies" />
                         </div>
                     </div>
-                    <div class="avatar shadow_white">
-                        <div class="w-20 rounded">
-                            <img src="https://placeimg.com/192/192/people" />
+
+                    <div>
+                        <div class="shadow_white rounded flex justify-center items-center p-2 md:p-0">
+                            <img src="https://placeimg.com/192/192/people" className='w-auto' alt="technologies" />
                         </div>
                     </div>
-                    <div class="avatar shadow_white">
-                        <div class="w-20 rounded">
-                            <img src="https://placeimg.com/192/192/people" />
+
+                    <div>
+                        <div class="shadow_white rounded flex justify-center items-center p-2 md:p-0">
+                            <img src="https://placeimg.com/192/192/people" className='w-auto' alt="technologies" />
                         </div>
                     </div>
-                    <div class="avatar shadow_white">
-                        <div class="w-20 rounded">
-                            <img src="https://placeimg.com/192/192/people" />
+
+                    <div>
+                        <div class="shadow_white rounded flex justify-center items-center p-2 md:p-0">
+                            <img src="https://placeimg.com/192/192/people" className='w-auto' alt="technologies" />
                         </div>
                     </div>
-                    <div class="avatar shadow_white">
-                        <div class="w-20 rounded">
-                            <img src="https://placeimg.com/192/192/people" />
+
+                    <div>
+                        <div class="shadow_white rounded flex justify-center items-center p-2 md:p-0">
+                            <img src="https://placeimg.com/192/192/people" className='w-auto' alt="technologies" />
                         </div>
                     </div>
-                    <div class="avatar shadow_white">
-                        <div class="w-20 rounded">
-                            <img src="https://placeimg.com/192/192/people" />
+
+                    <div>
+                        <div class="shadow_white rounded flex justify-center items-center p-2 md:p-0">
+                            <img src="https://placeimg.com/192/192/people" className='w-auto' alt="technologies" />
                         </div>
                     </div>
-                    <div class="avatar shadow_white">
-                        <div class="w-20 rounded">
-                            <img src="https://placeimg.com/192/192/people" />
+
+                    <div>
+                        <div class="shadow_white rounded flex justify-center items-center p-2 md:p-0">
+                            <img src="https://placeimg.com/192/192/people" className='w-auto' alt="technologies" />
                         </div>
                     </div>
-                    <div class="avatar shadow_white">
-                        <div class="w-20 rounded">
-                            <img src="https://placeimg.com/192/192/people" />
+
+                    <div>
+                        <div class="shadow_white rounded flex justify-center items-center p-2 md:p-0">
+                            <img src="https://placeimg.com/192/192/people" className='w-auto' alt="technologies" />
                         </div>
                     </div>
-                    <div class="avatar shadow_white">
-                        <div class="w-20 rounded">
-                            <img src="https://placeimg.com/192/192/people" />
+
+                    <div>
+                        <div class="shadow_white rounded flex justify-center items-center p-2 md:p-0">
+                            <img src="https://placeimg.com/192/192/people" className='w-auto' alt="technologies" />
                         </div>
                     </div>
-                    <div class="avatar shadow_white">
-                        <div class="w-20 rounded">
-                            <img src="https://placeimg.com/192/192/people" />
+
+                    <div>
+                        <div class="shadow_white rounded flex justify-center items-center p-2 md:p-0">
+                            <img src="https://placeimg.com/192/192/people" className='w-auto' alt="technologies" />
                         </div>
                     </div>
+
+
                 </div>
             </div>
         </section>
