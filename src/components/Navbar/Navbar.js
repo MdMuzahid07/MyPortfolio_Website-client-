ef0eebc93d4ccbcf2d7db34b638976c652cf0bbf
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-08-29 11:53:08 +0600

    navbar UI/UX updated

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,8 +6,8 @@ const Navbar = ({ children }) => {
         <nav>
             <div className="drawer drawer-mobile">
                 <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-                <div className="drawer-content bg-slate-100">
-                    <label for="my-drawer-2" className="fixed top-5 left-5 z-50 p-2 bg-primary border-2 border-secondary drop-shadow text-secondary rounded-full lg:hidden">
+                <div className="drawer-content bg-accent">
+                    <label for="my-drawer-2" className="fixed top-5 left-5 z-50 p-2 bg-accent border-2 border-secondary drop-shadow text-secondary rounded-full lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-10 h-10 stroke-current font-bold"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                     </label>
                     {/* <!-- Page content here --> */}
@@ -15,26 +15,30 @@ const Navbar = ({ children }) => {
                 </div>
                 <div className="drawer-side">
                     <label for="my-drawer-2" className="drawer-overlay" />
-                    <ul className="menu overflow-y-auto w-80 text-base-content  bg-black text-white font-bold">
+                    <ul className="menu overflow-y-auto w-80 text-base-content  bg-primary font-bold uppercase border-r border-dashed border-secondary">
                         {/* <!-- Sidebar content here --> */}
-                        <li>
-                            <a href="/#">Home</a>
+                        <li className='text-5xl text-white font-bold capitalize my-10'>
+                            <h2>Muzahid</h2>
                         </li>
 
                         <li>
-                            <a href="/#">About</a>
+                            <a href="/#" className='text-white'>Home</a>
                         </li>
 
                         <li>
-                            <a href="/#">Services</a>
+                            <a href="/#" className='text-white'>About</a>
                         </li>
 
                         <li>
-                            <a href="/#">Portfolio</a>
+                            <a href="/#" className='text-white'>Services</a>
                         </li>
 
                         <li>
-                            <a href="/#">Contact</a>
+                            <a href="/#" className='text-white'>Portfolio</a>
+                        </li>
+
+                        <li>
+                            <a href="/#" className='text-white'>Contact</a>
                         </li>
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,31 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#000",
+
+          "secondary": "#FF003A",
+
+          "accent": "#030206",
+
+          "neutral": "#3D4451",
+
+          "base-100": "#FFFFFF",
+
+          "info": "#3ABFF8",
+
+          "success": "#36D399",
+
+          "warning": "#FBBD23",
+
+          "error": "#F87272",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
 }
