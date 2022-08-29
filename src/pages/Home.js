284701a9052f468011a6844503cbe677299cd4f8
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-08-29 16:17:16 +0600

    working on homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,16 +2,16 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <main className='max-w-6xl mx-auto px-3 md:px-0'>
-            <div class="grid lg:grid-cols-2 bg-neutral text-secondary rounded-none w-full card-h mt-7">
+        <main className='max-w-6xl mx-auto px-3 lg:px-0 flex justify-center items-center min-h-screen'>
+            <div className="grid lg:grid-cols-2 bg-primary text-secondary rounded-none w-full">
                 <div>
-                    <img src="https://placeimg.com/400/400/arch" alt="profile_img" className='profile_img' />
+                    <img src="https://placeimg.com/400/400/arch" alt="profile_img" className='profile_img rounded-lg' />
                 </div>
-                <div class="text-secondary p-4">
-                    <h2>New album is released!</h2>
+                <div className="text-secondary p-4">
+                    <h2 className='text-xl font-bold capitalize'>console.log( hello world! )</h2>
                     <p>Click the button to listen on Spotiwhy app.</p>
-                    <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Listen</button>
+                    <div>
+                        <button className="btn btn-wide btn-outline btn-secondary mt-7">Download Resume</button>
                     </div>
                 </div>
             </div>
