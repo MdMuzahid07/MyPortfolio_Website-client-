93cf7ff76e6ca04617095e6616564f00a3ff9da0
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-08-29 10:38:32 +0600

    navbar UI/UX updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer>
-
+            <h2 className='text-2xl text-white'>this is footer</h2>
         </footer>
     );
 };
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,25 +1,47 @@
 import React from 'react';
+import Footer from '../Footer/Footer';
 
 const Navbar = ({ children }) => {
     return (
         <nav>
-            <div class="drawer drawer-mobile">
-                <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-                <div class="drawer-content bg-slate-100">
-                    <label for="my-drawer-2" class="fixed top-5 left-5 bg-black text-primary p-2 z-50 rounded lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-7 h-7 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+            <div className="drawer drawer-mobile">
+                <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content bg-slate-100">
+                    <label for="my-drawer-2" className="fixed top-5 left-5 z-50 p-2 bg-primary border-2 border-secondary drop-shadow text-secondary rounded-full lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-10 h-10 stroke-current font-bold"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                     </label>
                     {/* <!-- Page content here --> */}
                     {children}
                 </div>
-                <div class="drawer-side">
-                    <label for="my-drawer-2" class="drawer-overlay" />
-                    <ul class="menu p-4 overflow-y-auto w-80 text-base-content  bg-black text-white sticky top-0 left-0">
+                <div className="drawer-side">
+                    <label for="my-drawer-2" className="drawer-overlay" />
+                    <ul className="menu overflow-y-auto w-80 text-base-content  bg-black text-white font-bold">
                         {/* <!-- Sidebar content here --> */}
-                        <li><a>Sidebar Item 1</a></li>
-                        <li><a>Sidebar Item 2</a></li>
-                    </ul>
+                        <li>
+                            <a href="/#">Home</a>
+                        </li>
 
+                        <li>
+                            <a href="/#">About</a>
+                        </li>
+
+                        <li>
+                            <a href="/#">Services</a>
+                        </li>
+
+                        <li>
+                            <a href="/#">Portfolio</a>
+                        </li>
+
+                        <li>
+                            <a href="/#">Contact</a>
+                        </li>
+
+
+                        <li className='absolute bottom-5 hidden md:block'>
+                            <Footer />
+                        </li>
+                    </ul>
                 </div>
             </div>
         </nav>
