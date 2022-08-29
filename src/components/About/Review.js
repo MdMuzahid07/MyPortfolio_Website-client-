f590ebba8733545203d21e6c30d0ed582a6b003a
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-08-29 23:37:35 +0600

    testimonial slider, react slick configed

--- src/components/About/Review.js
+++ src/components/About/Review.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 
 const Review = () => {
@@ -40,35 +42,32 @@ const Review = () => {
     return (
         <div className='text-secondary my-20'>
             <h2 className='text-3xl font-bold underline mb-7'>Testimonials</h2>
-            <div>
-                <h2> Responsive </h2>
-                <Slider {...settings}>
-                    <div>
-                        <h3>1</h3>
-                    </div>
-                    <div>
-                        <h3>2</h3>
-                    </div>
-                    <div>
-                        <h3>3</h3>
-                    </div>
-                    <div>
-                        <h3>4</h3>
-                    </div>
-                    <div>
-                        <h3>5</h3>
-                    </div>
-                    <div>
-                        <h3>6</h3>
-                    </div>
-                    <div>
-                        <h3>7</h3>
-                    </div>
-                    <div>
-                        <h3>8</h3>
-                    </div>
-                </Slider>
-            </div>
+            <Slider {...settings}>
+                <div>
+                    <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum reprehenderit quod cum beatae. Dolore aliquid illo repellendus? Quibusdam, alias expedita.</h2>
+                </div>
+                <div>
+                    <h3>2</h3>
+                </div>
+                <div>
+                    <h3>3</h3>
+                </div>
+                <div>
+                    <h3>4</h3>
+                </div>
+                <div>
+                    <h3>5</h3>
+                </div>
+                <div>
+                    <h3>6</h3>
+                </div>
+                <div>
+                    <h3>7</h3>
+                </div>
+                <div>
+                    <h3>8</h3>
+                </div>
+            </Slider>
         </div>
     );
 };
