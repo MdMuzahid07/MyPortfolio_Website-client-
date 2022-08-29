dee49daff6cbac880798157f3885b444ee6c85c8
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-08-29 19:23:44 +0600

    install react typed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,13 +8,13 @@ const Home = () => {
                 <div>
                     <img src="https://placeimg.com/400/400/arch" alt="profile_img" className='profile_img rounded-lg' />
                 </div>
-                <div className="text-secondary p-4">
+                <div className="text-secondary p-4 relative">
                     <h2 className='text-xl font-bold my-10'>console.log( hello world! )</h2>
                     <h2 className='text-3xl'>I'm</h2>
                     <h2 className='text-4xl font-bold my-4'>Md. Muzahid</h2>
                     <TypingText />
-                    <div>
-                        <button className="btn btn-wide btn-outline btn-secondary mt-7">Download Resume</button>
+                    <div className='absolute bottom-32 left-5'>
+                        <button className="btn btn-wide btn-outline btn-secondary ">Download Resume</button>
                     </div>
                 </div>
             </div>
