c43ae52243b5fa7cde19dab9ceee8c07515eae76
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-08-29 21:30:54 +0600

    about section UI/UX updated

--- src/components/About/About_Me.js
+++ src/components/About/About_Me.js
@@ -5,16 +5,24 @@ const About_Me = () => {
         <section className='text-secondary'>
             <h2 className='text-3xl font-bold underline mb-7'>About Me</h2>
             <div>
-                <div className="grid lg:grid-cols-2 bg-primary text-secondary rounded-none w-full mt-20">
+                <div className="grid lg:grid-cols-2 bg-primary text-secondary rounded-none w-full mt-7">
                     <div>
-                        <img src="https://img.freepik.com/free-vector/men-success-laptop-relieve-work-from-home-computer-great_10045-646.jpg?w=740&t=st=1661782352~exp=1661782952~hmac=dcf5f61124dc5f7f0ae495ffbb12ee7ea51257006982d566699db1c4fed27242" alt="profile_image" className='w-full h-96 border-2 border-secondary rounded' />
+                        <img src="https://img.freepik.com/free-vector/men-success-laptop-relieve-work-from-home-computer-great_10045-646.jpg?w=740&t=st=1661782352~exp=1661782952~hmac=dcf5f61124dc5f7f0ae495ffbb12ee7ea51257006982d566699db1c4fed27242" alt="profile_image" className='about_img border-2 border-secondary rounded' />
                     </div>
                     <div className="text-secondary p-4">
-                        <h2 className='text-xl font-bold mb-7'>console.log( hello world! )</h2>
-                        <h2 className='text-3xl mt-3'>Hi there</h2>
-                        <p className='text-md font-bold mt-7'>{`
+                        <h2 className='text-xl font-bold my-10'>console.log( hello world! )</h2>
+                        <h2 className='text-3xl mt-7'>Hi there</h2>
+                        <p className='text-md font-bold mt-4'>{`
                             <> I'm a Web Developer, Works with MERN Stack, Front-end ,Back-end Development, and WordPress Website Design. My future goal is to be a full-stack native app developer. and I want to be a master in JavaScript.My first rule is, to be a good person,then a good Developer. </>
                             `}</p>
+
+                        <div className='mt-10'>
+                            <p>Birthday: Feb 06,2000</p>
+                            <p>From: Dhaka, Bangladesh</p>
+                            <p>Language: English, Hindi, Bengali</p>
+                            <p>E-Mail : MdMuzahid7396@gmail.com</p>
+                            <p>Freelance : Available</p>
+                        </div>
                         <div className='mt-10'>
                             <button className="btn btn-wide btn-outline btn-secondary ">Hire Me</button>
                         </div>
