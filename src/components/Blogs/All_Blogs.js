ad31711695c44a4345993245fca37521263a24e0
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-08-30 15:32:49 +0600

    blog page card UI complete

--- src/components/Blogs/All_Blogs.js
+++ src/components/Blogs/All_Blogs.js
@@ -1,10 +1,159 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const All_Blogs = () => {
     return (
         <section className='bg-primary p-5 rounded flex justify-center items-center min-h-screen'>
-            <div>
-                <h2>All Blogs</h2>
+            <div className='grid md:grid-cols-3 lg:grid-cols-4 gap-5'>
+
+                <div class="card h-80 card-compact bg-primary rounded shadow_red">
+                    <figure>
+                        <img src="https://placeimg.com/400/225/arch" alt="project_logo" />
+                    </figure>
+                    <div class="card-body">
+                        <div>
+                            <h2 className="card-title">title</h2>
+                        </div>
+                        <div class="card-actions absolute -left-14 hover:left-2 transition bottom-5">
+                            <Link to="/" className="btn btn-secondary btn-outline rounded">Read Blog</Link>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card h-80 card-compact bg-primary rounded shadow_red">
+                    <figure>
+                        <img src="https://placeimg.com/400/225/arch" alt="project_logo" />
+                    </figure>
+                    <div class="card-body">
+                        <div>
+                            <h2 className="card-title">title</h2>
+                        </div>
+                        <div class="card-actions absolute -left-14 hover:left-2 transition bottom-5">
+                            <Link to="/project" className="btn btn-secondary btn-outline rounded">Read Blog</Link>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="card h-80 card-compact bg-primary rounded shadow_red">
+                    <figure>
+                        <img src="https://placeimg.com/400/225/arch" alt="project_logo" />
+                    </figure>
+                    <div class="card-body">
+                        <div>
+                            <h2 className="card-title">title</h2>
+                        </div>
+                        <div class="card-actions absolute -left-14 hover:left-2 transition bottom-5">
+                            <Link to="/project" className="btn btn-secondary btn-outline rounded">Read Blog</Link>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="card h-80 card-compact bg-primary rounded shadow_red">
+                    <figure>
+                        <img src="https://placeimg.com/400/225/arch" alt="project_logo" />
+                    </figure>
+                    <div class="card-body">
+                        <div>
+                            <h2 className="card-title">title</h2>
+                        </div>
+                        <div class="card-actions absolute -left-14 hover:left-2 transition bottom-5">
+                            <Link to="/project" className="btn btn-secondary btn-outline rounded">Read Blog</Link>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="card h-80 card-compact bg-primary rounded shadow_red">
+                    <figure>
+                        <img src="https://placeimg.com/400/225/arch" alt="project_logo" />
+                    </figure>
+                    <div class="card-body">
+                        <div>
+                            <h2 className="card-title">title</h2>
+                        </div>
+                        <div class="card-actions absolute -left-14 hover:left-2 transition bottom-5">
+                            <Link to="/project" className="btn btn-secondary btn-outline rounded">Read Blog</Link>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="card h-80 card-compact bg-primary rounded shadow_red">
+                    <figure>
+                        <img src="https://placeimg.com/400/225/arch" alt="project_logo" />
+                    </figure>
+                    <div class="card-body">
+                        <div>
+                            <h2 className="card-title">title</h2>
+                        </div>
+                        <div class="card-actions absolute -left-14 hover:left-2 transition bottom-5">
+                            <Link to="/project" className="btn btn-secondary btn-outline rounded">Read Blog</Link>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="card h-80 card-compact bg-primary rounded shadow_red">
+                    <figure>
+                        <img src="https://placeimg.com/400/225/arch" alt="project_logo" />
+                    </figure>
+                    <div class="card-body">
+                        <div>
+                            <h2 className="card-title">title</h2>
+                        </div>
+                        <div class="card-actions absolute -left-14 hover:left-2 transition bottom-5">
+                            <Link to="/project" className="btn btn-secondary btn-outline rounded">Read Blog</Link>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="card h-80 card-compact bg-primary rounded shadow_red">
+                    <figure>
+                        <img src="https://placeimg.com/400/225/arch" alt="project_logo" />
+                    </figure>
+                    <div class="card-body">
+                        <div>
+                            <h2 className="card-title">title</h2>
+                        </div>
+                        <div class="card-actions absolute -left-14 hover:left-2 transition bottom-5">
+                            <Link to="/project" className="btn btn-secondary btn-outline rounded">Read Blog</Link>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="card h-80 card-compact bg-primary rounded shadow_red">
+                    <figure>
+                        <img src="https://placeimg.com/400/225/arch" alt="project_logo" />
+                    </figure>
+                    <div class="card-body">
+                        <div>
+                            <h2 className="card-title">title</h2>
+                        </div>
+                        <div class="card-actions absolute -left-14 hover:left-2 transition bottom-5">
+                            <Link to="/project" className="btn btn-secondary btn-outline rounded">Read Blog</Link>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="card h-80 card-compact bg-primary rounded shadow_red">
+                    <figure>
+                        <img src="https://placeimg.com/400/225/arch" alt="project_logo" />
+                    </figure>
+                    <div class="card-body">
+                        <div>
+                            <h2 className="card-title">title</h2>
+                        </div>
+                        <div class="card-actions absolute -left-14 hover:left-2 transition bottom-5">
+                            <Link to="/project" className="btn btn-secondary btn-outline rounded">Read Blog</Link>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </section>
     );
