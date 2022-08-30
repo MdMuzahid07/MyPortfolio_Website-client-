0372749b45a03671193a6f16a67039f72f3f8d1d
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-08-30 13:59:43 +0600

    working on blog page

---
+++ src/components/Blogs/All_Blogs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const All_Blogs = () => {
+    return (
+        <div className='bg-primary p-5 rounded'>
+            <h2>All Blogs</h2>
+        </div>
+    );
+};
+
+export default All_Blogs;
--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import All_Blogs from '../components/Blogs/All_Blogs';
 
 const Blogs = () => {
     return (
-        <div>
-            <h2>this is blog page</h2>
+        <div className='max-w-6xl mx-auto px-3 lg:px-0 my-10 md:my-7 text-secondary'>
+            <h2 className='text-3xl font-bold underline custom-m-b-5'>My Blogs</h2>
+
+            <div>
+                <All_Blogs />
+            </div>
+
         </div>
     );
 };
