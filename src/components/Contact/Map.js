2f68c054756640a5d2dae2557a6e07c0b7a96eae
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-09-01 12:13:18 +0600

    MAP aded in contact page

--- src/components/Contact/Map.js
+++ src/components/Contact/Map.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Map = () => {
     return (
-        <div>
-            <h2>this is map</h2>
-        </div>
+        <section>
+            <iframe className='w-full h-72 rounded-xl' title='map' frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=%20Dhaka+()&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><script type='text/javascript' src='https://embedmaps.com/google-maps-authorization/script.js?id=26d73d4e40650b3f73f63d4cb18b61118bb0df8c'></script>
+        </section>
     );
 };
 
