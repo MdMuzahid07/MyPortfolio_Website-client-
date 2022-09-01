ee35cc803f3a0c2740485987ee37eccfce719757
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-09-01 11:02:22 +0600

    working on contact page UI/UX

---
+++ src/components/Contact/ContactForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactForm = () => {
+    return (
+        <section className='text-secondary'>
+            <h2>contact form</h2>
+        </section>
+    );
+};
+
+export default ContactForm;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import ContactForm from '../components/Contact/ContactForm';
 
 const Contact = () => {
     return (
-        <div>
-            <h2>this is contact page</h2>
+        <div className='max-w-6xl mx-auto px-3 lg:px-0 my-10 md:my-0 flex justify-center items-center min-h-screen'>
+            <ContactForm />
         </div>
     );
 };
