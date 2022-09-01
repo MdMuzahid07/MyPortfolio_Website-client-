796343a0ea6ee843f62f04775e16724029d50dc8
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-09-01 13:04:12 +0600

    project card UI update

--- src/components/Portfolio/Project.js
+++ src/components/Portfolio/Project.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Project = ({ app }) => {
 
-    const { projectName, technology, description, logo } = app;
+    const { projectName, technology, logo } = app;
     console.log(app)
     return (
         <div class="card h-96 card-compact bg-primary rounded shadow_red">
@@ -13,9 +13,10 @@ const Project = ({ app }) => {
             <div class="card-body">
                 <div>
                     <h2 className="card-title">{projectName}</h2>
-                    <p>Title</p>
-                    <p>Technology: {technology}</p>
-                    <p>Description: {description}</p>
+                    <div className='text-xs'>
+                        <p>Title: E-commerce website</p>
+                        <p>Technology: {technology}</p>
+                    </div>
                 </div>
                 <div class="card-actions absolute -left-14 hover:left-2 transition bottom-5">
                     <Link to="/project" className="btn btn-wide  btn-secondary btn-outline rounded">Project Details</Link>
